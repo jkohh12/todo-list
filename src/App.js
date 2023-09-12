fb562fc3d1d397f6c5d77da3d03850b47a56a776
jkohh12 <jkohh.12@gmail.com>
2023-09-11 18:48:52 -0700

    completed todo list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import FruitForm from './components/FruitForm';
 import TodoForm from './components/TodoForm';
-import TodoDisplay from './components/TodoDisplay';
 import './App.css';
 
 function App() {
@@ -10,15 +9,39 @@ function App() {
 
 
   const addNewTask = ( addTask ) => {
-    setNewTask([...newTask, addTask]);
+    if(addTask == '') return;
+    const taskToAdd = { text: addTask, checked: false}; //setting properties to each item added
+    // text property and checked property
+    setNewTask([...newTask, taskToAdd]);
   }
 
-  const deleteCurrentTask = ( deleteTask ) => {
-    const newTaskList = newTask.filter((task) => task != deleteTask);
+
+
+  const deleteCurrentTask = (index) => {
+
+    const newTaskList = newTask.filter((_, i) => i != index);
+    // (represented by _, which is a convention used when you don't intend to use the parameter) 
+    // and the index of the current element in the array (represented by i).
+    // If the condition i != index is true for an element, 
+    // it will be included in the newTaskList array.
+    // basically filter to only what isnt i == index
     setNewTask(newTaskList);
+
+
   }
+
+  const handleCheck = (index) => {
+    const updatedItems = [...newTask]; //copying list
+    updatedItems[index].checked = !updatedItems[index].checked; //negates current value of checked property
+    //if it was true, it becomes false, false to true
+    setNewTask(updatedItems);
+  }
+  
   return (
     <>
+    <h1 className = 'header'>
+      To do List:
+    </h1>
 
     <div className = 'App'>
       <TodoForm onNewTask = { addNewTask }/>
@@ -29,7 +52,20 @@ function App() {
     <div className = 'display'>
       {
           newTask.map((task, index) => (
-            <TodoDisplay deleteNewTask = { deleteCurrentTask } key = {index} task = {task} />
+            <div key = {index} style = {{ textDecoration: task.checked ? 'line-through' : 'none'}}>
+              <span>
+              {task.text}
+              </span>
+              <label className = 'checkBox'>
+                <input checked = {task.checked} type = "checkbox" onChange= {() => handleCheck(index)}/>
+                <button type = "button" className = 'delete' onClick={() => deleteCurrentTask(index)}>
+                Delete
+                </button>
+              </label>
+              
+
+            </div>
+          
           ))
       }
     </div>
